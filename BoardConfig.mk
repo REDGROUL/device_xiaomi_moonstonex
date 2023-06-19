75cd7dd3303fa3ac50f1079a9df436970401c6b9
REDGROUL <38361748+REDGROUL@users.noreply.github.com>
2023-06-19 11:00:56 +0300

    disable init veux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,8 +79,8 @@ ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/hidl/manifest_nfc.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_veux
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_veux
+#TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_veux
+#TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_veux
 
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
